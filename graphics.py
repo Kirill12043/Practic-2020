088cd9b2c4b054db17357c95f801fff6094f5450
Kirill12043 <Gromoboi1204@yandex.ru>
2020-07-06 19:02:14 +0300

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from influxdb import InfluxDBClient
+ importconstants as CS
+
+
+if __name__ = =  "__main__":
+     
+    client = InfluxDBClient(host='localhost', port=8086, database="tsdb")
+    fields = { x: [] for x in CS.VARS_LIST}
+    time = []
+
+    def animate(i):
+        qr =  client.query('SELECT * FROM "measurement_tsdb" GROUP BY "time()"', epoch= 's') 
+        tm_ = qr.get_points() 
+        tm = [ x for x in tm_] 
+        if len(tm) = =  0: 
+            return
+
+        time.clear()
+        for a in fields:
+            fields[ a].clear()
+        for a in tm:
+            time.append( int(a["time"]))
+            for b in CS.VARS_LIST:
+                fields[ b].append( int(a[ b]))
+        plt.cla()
+        for b in CS.VARS_LIST:
+            plt.plot(time, fields[ b], label= b)
+        plt.tight_layout()
+        plt.legend( loc= 'upper right')
+
+
+    ani = FuncAnimation(plt.gcf(), animate, interval= 1000)
+    plt.tight_layout()
+    plt.show()
