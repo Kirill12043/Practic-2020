be723d78c7c2a20fceae9e898e9deb3dc974731c
Kirill12043 <Gromoboi1204@yandex.ru>
2020-07-15 16:20:36 +0300

    Add files via upload

---
+++ Время/prog_constants.py
@@ -0,0 +1,8 @@
+VARS_LIST = [
+   "a", "b", "c", "d", "e", "f", "g", "h", "k", "l"
+]
+
+MQTT_ADDR = 'mqtt'
+INFLUXDB_ADDR = 'influx'
+INFLUXDB_DB_NAME = 'tsdb'
+INFLUXDB_MEAS_NAME = 'measurement_tsdb'
