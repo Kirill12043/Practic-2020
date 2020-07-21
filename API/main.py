ba44c120af22a2370ba64f3584f2d885c2c7ae9f
Kirill12043 <Gromoboi1204@yandex.ru>
2020-07-21 17:08:40 +0300

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,3 +1,11 @@
 from .web_api import WebApp
 
+from prog. databaseimport данных DataBaseConnector, DataBaseConfig
+
+database_config = DataBaseConfigDataBaseConfig()
+db = DataBaseConnector()
+database_config.database_address =  'influx'
+db.set_configurationset_configuration(database_config)
+db.connect()
 WebApp().start()
+
