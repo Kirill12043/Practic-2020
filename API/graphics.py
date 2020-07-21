79fc017210844d39f7451c15fe3446c5d6a6acd8
Kirill12043 <Gromoboi1204@yandex.ru>
2020-07-21 17:16:34 +0300

    Update graphics.py

--- API/graphics.py
+++ API/graphics.py
@@ -1,36 +1,58 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from influxdb import InfluxDBClient
- importconstants as CS
+from database import DataBaseConfig, DataBaseConnector
 
 
-if __name__ = =  "__main__":
-     
-    client = InfluxDBClient(host='localhost', port=8086, database="tsdb")
-    fields = { x: [] for x in CS.VARS_LIST}
-    time = []
+class VariableContainer:
+    def __init__(self):
+        self._time_measure = []
+        self._vars_measure = []
+
+    def set_values(self, time_m, vars_m):
+        self._time_measure.append(float(time_m))
+        self._vars_measure.append(float(vars_m))
+
+    def get_vals(self):
+        return self._time_measure, self._vars_measure
+
+    def clear_vars(self):
+        self._time_measure.clear()
+        self._vars_measure.clear()
+
+
+if __name__ == "__main__":
+    variables = {}
+    database_config = DataBaseConfig()
+    database_config.database_name = "tsdb"
+    database_config.database_measure_name = "measurement_tsdb"
+    database = DataBaseConnector()
+    database.set_configuration(database_config)
+    database.connect()
 
     def animate(i):
-        qr =  client.query('SELECT * FROM "measurement_tsdb" GROUP BY "time()"', epoch= 's') 
-        tm_ = qr.get_points() 
-        tm = [ x for x in tm_] 
-        if len(tm) = =  0: 
+
+        tm = database.get_data()
+        if len(tm) == 0:
             return
 
-        time.clear()
-        for a in fields:
-            fields[ a].clear()
+        for a in variables:
+            variables[a].clear_vars()
+
         for a in tm:
-            time.append( int(a["time"]))
-            for b in CS.VARS_LIST:
-                fields[ b].append( int(a[ b]))
+            timen = a.pop("time")
+            for b in a:
+                if a[b] is None:
+                    continue
+                if variables.get(b, None) is None:
+                    variables[b] = VariableContainer()
+                variables[b].set_values(timen, a[b])
         plt.cla()
-        for b in CS.VARS_LIST:
-            plt.plot(time, fields[ b], label= b)
+        for b in variables:
+            plt.plot(*(variables[b].get_vals()), label=b)
         plt.tight_layout()
-        plt.legend( loc= 'upper right')
+        plt.legend(loc='upper right')
 
 
-    ani = FuncAnimation(plt.gcf(), animate, interval= 1000)
+    ani = FuncAnimation(plt.gcf(), animate, interval=500)
     plt.tight_layout()
     plt.show()
